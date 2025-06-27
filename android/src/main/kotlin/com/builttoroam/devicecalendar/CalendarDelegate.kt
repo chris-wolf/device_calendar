45af4a99d06d0b555b5d086ffae50b3d0f78b864
Christopher Wolf <>
2025-06-27 09:43:35 +0200

    small import fix

--- android/src/main/kotlin/com/builttoroam/devicecalendar/CalendarDelegate.kt
+++ android/src/main/kotlin/com/builttoroam/devicecalendar/CalendarDelegate.kt
@@ -42,7 +42,6 @@ import org.dmfs.rfc5545.recur.RecurrenceRule as Rrule
 import android.provider.CalendarContract.Colors
 import androidx.collection.SparseArrayCompat
 import android.content.ContentProviderResult
-import android.content.ContentResolver
 
 private const val RETRIEVE_CALENDARS_REQUEST_CODE = 0
 private const val RETRIEVE_EVENTS_REQUEST_CODE = RETRIEVE_CALENDARS_REQUEST_CODE + 1
